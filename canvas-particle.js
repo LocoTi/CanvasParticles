2a15467ca8d529ef9951926733eab6fa7f7d1c20
locoti <xiaoyao9610@hotmail.com>
2016-08-17 10:32:22 +0800

    -m封装，调用函数并传入配置参数即可 ; ;

--- canvas-particle.js
+++ canvas-particle.js
@@ -1,14 +1,13 @@
-(function(){
-	var flag = false;
+var CanvasParticle = (function(){
 	function getElementByTag(name){
-		var doc = document;
-		return doc.getElementsByTagName(name);
+		return document.getElementsByTagName(name);
 	}
 	function getELementById(id){
-		var doc = document;
-		return doc.getElementById(id);
+		return document.getElementById(id);
 	}
+	// 根据传入的config初始化画布
 	function canvasInit(canvasConfig){
+		canvasConfig = canvasConfig || {};
 		var html = getElementByTag("html")[0];
 		var body = getElementByTag("body")[0];
 		var canvasDiv = getELementById("canvas-particle");
@@ -17,16 +16,18 @@
 		var canvas = {
 			element: canvasObj,
 			points : [],
+			// 默认配置
 			config: {
-				vx: canvasConfig.vx || 3,
-				vy:  canvasConfig.vy || 3,
+				vx: canvasConfig.vx || 4,
+				vy:  canvasConfig.vy || 4,
 				height: canvasConfig.height || 2,
 				width: canvasConfig.width || 2,
 				count: canvasConfig.count || 100,
-				color: canvasConfig.color || "65,100,130",
-				stroke: canvasConfig.stroke || "40,125,80",
+				color: canvasConfig.color || "121, 162, 185",
+				stroke: canvasConfig.stroke || "130,255,255",
 				dist: canvasConfig.dist || 6000,
-				e_dist: canvasConfig.e_dist || 20000
+				e_dist: canvasConfig.e_dist || 20000,
+				max_conn: 10
 			}
 		};
 
@@ -36,20 +37,18 @@
 		}else{
 			return null;
 		}
-		// html.style.width = "100%";
-		// html.style.height = "100%";
-		// body.style.width = "100%";
-		// body.style.height = "100%";
+
 		body.style.padding = "0";
 		body.style.margin = "0";
-		body.replaceChild(canvas.element, canvasDiv);
+		// body.replaceChild(canvas.element, canvasDiv);
+		body.appendChild(canvas.element);
 
-		canvas.element.style = "position: absolute; z-index: -1;";
+		canvas.element.style = "position: absolute; top: 0; left: 0; z-index: -1;";
 		canvasSize(canvas.element);
 		window.onresize = function(){
 			canvasSize(canvas.element);
 		}
-		document.onmousemove = function(e){
+		body.onmousemove = function(e){
 			var event = window.event || e;
 			canvas.mouse = {
 				x: event.clientX,
@@ -57,11 +56,12 @@
 			}
 		}
 		document.onmouseleave = function(){
+			body.onmousemove = null;
 			canvas.mouse = undefined;
 		}
 		setInterval(function(){
 			drawPoint(canvas);
-		}, 20);
+		}, 40);
 	}
 
 	// 设置canvas大小
@@ -73,7 +73,6 @@
 	// 画点
 	function drawPoint(canvas){
 		var context = canvas.context,
-			points = new Array(),
 			point,
 			dist;
 		context.clearRect(0, 0, canvas.element.width, canvas.element.height);
@@ -81,6 +80,7 @@
 		context.fillStyle = "rgb("+ canvas.config.color +")";
 		for(var i = 0, len = canvas.config.count; i < len; i++){
 			if(canvas.points.length != canvas.config.count){
+				// 初始化所有点
 				point = {
 					x: Math.floor(Math.random() * canvas.element.width),
 					y: Math.floor(Math.random() * canvas.element.height),
@@ -88,19 +88,15 @@
 					vy: canvas.config.vy / 2 - Math.random() * canvas.config.vy
 				}
 			}else{
+				// 处理球的速度和位置，并且做边界处理
 				point =	borderPoint(canvas.points[i], canvas);
 			}
-			context.fillRect(point.x - 1, point.y - 1, canvas.config.width, canvas.config.height);
+			context.fillRect(point.x - canvas.config.width / 2, point.y - canvas.config.height / 2, canvas.config.width, canvas.config.height);
 
 			canvas.points[i] = point;
 		}
 		drawLine(context, canvas, canvas.mouse);
 		context.closePath();
-
-		if(!flag){
-			flag = true;
-			console.log(canvas);
-		}
 	}
 
 	// 边界处理
@@ -127,13 +123,17 @@
 	function drawLine(context, canvas, mouse){
 		context = context || canvas.context;
 		for(var i = 0, len = canvas.config.count; i < len; i++){
-			for(var j = 0, len = canvas.config.count; j < len; j++){
-				// point to point
+			// 初始化最大连接数
+			canvas.points[i].max_conn = 0;
+			// point to point
+			for(var j = 0; j < len; j++){
 				if(i != j){
 					dist = Math.round(canvas.points[i].x - canvas.points[j].x) * Math.round(canvas.points[i].x - canvas.points[j].x) + 
 							Math.round(canvas.points[i].y - canvas.points[j].y) * Math.round(canvas.points[i].y - canvas.points[j].y);
-					if(dist <= canvas.config.dist){
-
+					// 两点距离小于吸附距离，而且小于最大连接数，则画线
+					if(dist <= canvas.config.dist && canvas.points[i].max_conn <canvas.config.max_conn){
+						canvas.points[i].max_conn++;
+						// 距离越远，线条越细，而且越透明
 						context.lineWidth = 0.5 - dist / canvas.config.dist;
 						context.strokeStyle = "rgba("+ canvas.config.stroke + ","+ (1 - dist / canvas.config.dist) +")"
 						context.beginPath();
@@ -144,6 +144,7 @@
 					}
 				}
 			}
+			// 如果鼠标进入画布
 			// point to mouse
 			if(mouse){
 				dist = Math.round(canvas.points[i].x - mouse.x) * Math.round(canvas.points[i].x - mouse.x) + 
@@ -155,7 +156,7 @@
 				}
 				if(dist <= canvas.config.dist){
 					context.lineWidth = 1;
-					context.strokeStyle = "rgba("+ canvas.config.stroke + ","+ (1 - dist / canvas.config.dist) +")"
+					context.strokeStyle = "rgba("+ canvas.config.stroke + ","+ (1 - dist / canvas.config.dist) +")";
 					context.beginPath();
 					context.moveTo(canvas.points[i].x, canvas.points[i].y);
 					context.lineTo(mouse.x, mouse.y);
@@ -164,6 +165,5 @@
 			}
 		}
 	}
-
-	canvasInit({});
+	return canvasInit;
 })();
